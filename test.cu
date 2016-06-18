77f4750f6943bc6477ffd32f90baae9640f7ab7b
AaronJoe <nisaac29@gmail.com>
2016-06-18 15:02:10 -0700

    updated gpu test

--- test.cu
+++ test.cu
@@ -1,30 +1,62 @@
 __global__ void add(int *a, int *b , int *result)
 {
-    *result= *a + *b;   
+    int index=blockIdx.x * blockDim.x + threadIdx.x;
+    *result[index]= *a[index] + *b[index];   
 }
+
+#define N (2048*2048)
+#define THREADS_PER_BLOCK 512
 
 int main()
 {
     int a,b,result;
     int *device_a,*device_b,*device_result;
-    int size=sizeof(int);
+    int size=sizeof(int)*N;
     
     cudaMalloc((void **)&device_a,size);
     cudaMalloc((void **)&device_b,size);
     cudaMalloc((void **)&device_result,size);
     
-    a=2;
-    b=7;
+    a=(int *)malloc(size);
+    b=(int *)malloc(size);
+    result=(int *)malloc(size);
+    
+    random_ints(a,N);
+    random_ints(b,N);
     
     cudaMemcpy(device_a,&a,size,cudaMemcpyHostToDevice);
     cudaMemcpy(device_b,&b,size,cudaMemcpyHostToDevice);
     
-    add<<<1,1>>>(device_a,device_b,device_result);
+    add<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(device_a,device_b,device_result);
     
     cudaMemcpy(&result,device_result,size,cudaMemcpyDeviceToHost);
-    
     cudaFree(device_a);
     cudaFree(device_b);
     cudaFree(device_result);
+    
+    free(a);
+    free(b);
+    free(result);
+    
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
