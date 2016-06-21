895e79f840f58c35e8625ffa0716aae70662bf2d
Aaron Zhou Qian <Aaron@Zhous-MacBook-Pro.local>
2016-06-21 16:24:37 -0700

    updated 3conv_layers.py

--- hypertfcnn.py
+++ hypertfcnn.py
@@ -78,13 +78,11 @@ def routine():
 
     sess.run(tf.initialize_all_variables())
     for i in range(20000):
-      batch = mnist.train.next_batch(50)
-      if i%100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g"%(i, train_accuracy))
+        batch = mnist.train.next_batch(50)
+        if i%100 == 0:
+          train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+          print("step %d, training accuracy %g"%(i, train_accuracy))
     
-      with tf.device('/gpu:0'):
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
     print("test accuracy %g"%accuracy.eval(feed_dict={
--- window_hypertfcnn.py
+++ window_hypertfcnn.py
@@ -75,14 +75,12 @@ def routine(W1_dim=[5,5,1,32],b1_dim=[32],W2_dim=[5, 5, 32, 64],b2_dim=[64]):
 
     sess.run(tf.initialize_all_variables())
     for i in range(20000):
-      batch = mnist.train.next_batch(50)
-      if i%100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], y_: batch[1], keep_prob: 1.0})
+        batch = mnist.train.next_batch(50)
+        if i%100 == 0:
+            train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
-    
-
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+        
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
     print("test accuracy %g"%accuracy.eval(feed_dict={
         x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
