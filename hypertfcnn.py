eeaaeb9f9cbcc368f3c22dc122f84f673933d0bd
Aaron Zhou Qian <Aaron@Zhous-MacBook-Pro.local>
2016-06-21 13:52:07 -0700

    hyperparameter tunning file

--- hypertfcnn.py
+++ hypertfcnn.py
@@ -4,6 +4,21 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist=input_data.read_data_sets('MNIST_data',one_hot=True)
 
+
+#----------------------------------------------#
+#The hyperparameters 
+#dimensions of the windows
+
+W_conv1_weight_variable_dim=[5,5,1,32]
+b_conv1_bias_variable_dim=W_conv1_weight_variable_dim[-1]
+
+W_conv2_weigth_variable_dim=[5, 5, 32, 64]
+b_conv2_bias_variable_dim=W_conv2_weigth_variable_dim[-1]
+
+
+#----------------------------------------------#
+
+
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
@@ -28,16 +43,19 @@ y_=tf.placeholder(tf.float32,shape=[None,10])
 
 x_image=tf.reshape(x,[-1,28,28,1])
 
+
+
+
 #first layer
-W_conv1=weight_variable([5,5,1,32])
-b_conv1=bias_variable([32])
+W_conv1=weight_variable(W_conv1_weight_variable_dim)
+b_conv1=bias_variable(b_conv1_bias_variable_dim)
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 #second layer
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable(W_conv2_weigth_variable_dim)
+b_conv2 = bias_variable(b_conv2_bias_variable_dim)
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
@@ -74,6 +92,7 @@ for i in range(20000):
 
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
 
 
 
