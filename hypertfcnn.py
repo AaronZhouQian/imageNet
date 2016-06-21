c8ee8e52ba4e3049039ad2166fd411756cde4818
Aaron Zhou Qian <Aaron@Zhous-MacBook-Pro.local>
2016-06-21 14:00:31 -0700

    changed hyperparam file

--- hypertfcnn.py
+++ hypertfcnn.py
@@ -37,50 +37,53 @@ def max_pool_2x2(x):
 
 sess=tf.InteractiveSession()
 #input data
-x=tf.placeholder(tf.float32,shape=[None,784])
-#correct labels
-y_=tf.placeholder(tf.float32,shape=[None,10])
+with tf.device('/gpu:0'):
+    x=tf.placeholder(tf.float32,shape=[None,784])
+    #correct labels
+    y_=tf.placeholder(tf.float32,shape=[None,10])
 
-x_image=tf.reshape(x,[-1,28,28,1])
+    x_image=tf.reshape(x,[-1,28,28,1])
 
 
 
 
-#first layer
-W_conv1=weight_variable(W_conv1_weight_variable_dim)
-b_conv1=bias_variable(b_conv1_bias_variable_dim)
+    #first layer
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+    W_conv1=weight_variable(W_conv1_weight_variable_dim)
+    b_conv1=bias_variable(b_conv1_bias_variable_dim)
 
-#second layer
-W_conv2 = weight_variable(W_conv2_weigth_variable_dim)
-b_conv2 = bias_variable(b_conv2_bias_variable_dim)
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+    #second layer
+    W_conv2 = weight_variable(W_conv2_weigth_variable_dim)
+    b_conv2 = bias_variable(b_conv2_bias_variable_dim)
 
-#Densely Connected Layer
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    #Densely Connected Layer
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
 
-#dropout
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-#readout layer
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+    #dropout
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    #readout layer
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    
 sess.run(tf.initialize_all_variables())
 for i in range(20000):
   batch = mnist.train.next_batch(50)
