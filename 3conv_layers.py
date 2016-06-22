3859a4fb4624cca6f76479230d788ec6682bc092
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-06-22 14:01:16 -0700

    test 4 conv layers

--- 3conv_layers.py
+++ 3conv_layers.py
@@ -94,7 +94,7 @@ def routine():
     
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-        print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 
 def main():
