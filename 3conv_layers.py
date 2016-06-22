bdc803c084128e3c0edc711ffefc929d9c46e51a
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-06-22 14:01:16 -0700

    test 4 conv layers

--- 3conv_layers.py
+++ 3conv_layers.py
@@ -16,12 +16,12 @@ W_conv3_weight_variable_dim=[5,5,64,64]
 b_conv3_bias_variable_dim=[64]
 
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
   
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
   
 
 def conv2d(x,W):
