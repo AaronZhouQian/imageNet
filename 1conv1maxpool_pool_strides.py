3a40f8937e0081d776c84481eaf50bc0dd1517a8
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-06-22 10:18:00 -0700

    added code for testing the stride parameter

--- 1conv1maxpool_pool_strides.py
+++ 1conv1maxpool_pool_strides.py
@@ -94,9 +94,10 @@ def routine(conv2d_stride_length=2,W1_dim=[5,5,1,32],b1_dim=[32],W2_dim=[5, 5, 3
 
 def main():
     for c2stride_length in [2,3,4,5,6]:
-        routine(conv2d_stride_length=c2stride_length)
-        print("the conv2d_stride_length is %d" %c2stride_length)
-        print("current time is: %s" %time.ctime())
+        for counter in range(3):
+            routine(conv2d_stride_length=c2stride_length)
+            print("the conv2d_stride_length is %d" %c2stride_length)
+            print("current time is: %s" %time.ctime())
 
 if __name__ == '__main__':
   main()
